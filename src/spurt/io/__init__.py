7a7a958f8977cc529768dba3bdbf69ca3de7cef0
piyushrpt <piyushrpt@users.noreply.github.com>
2024-03-27 18:09:03 -0700

    Add simple 3D readers

--- src/spurt/io/__init__.py
+++ src/spurt/io/__init__.py
@@ -1,6 +1,9 @@
 from ._dataset import InputStackDataset, OutputStackDataset
+from ._three_d import Irreg3DInput, Reg3DInput
 
 __all__ = [
     "InputStackDataset",
     "OutputStackDataset",
+    "Reg3DInput",
+    "Irreg3DInput",
 ]
