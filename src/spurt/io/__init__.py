0cbe1ed4ce28f6ff947abd7024c47324bc61eeb9
piyushrpt <piyushrpt@users.noreply.github.com>
2024-03-28 12:25:08 -0700

    Checking in raster

--- src/spurt/io/__init__.py
+++ src/spurt/io/__init__.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from ._interface import InputInterface, OutputInterface
 from ._three_d import Irreg3DInput, Reg3DInput
 
@@ -7,3 +9,27 @@ __all__ = [
     "Reg3DInput",
     "Irreg3DInput",
 ]
+
+
+def __getattr__(name: str) -> Any:
+    if name == "Raster":
+        # This module depends on `rasterio` so load it lazily to avoid an ImportError
+        # for users that don't need `Raster`.
+        from ._raster import Raster
+
+        return Raster
+
+    if name in __all__:
+        return globals()[name]
+
+    errmsg = f"module {__name__!r} has no attribute {name!r}"
+    raise AttributeError(errmsg)
+
+
+def __dir__() -> list[str]:
+    try:
+        from ._raster import Raster
+    except ModuleNotFoundError:
+        return __all__
+    else:
+        return sorted([*__all__, "Raster"])
