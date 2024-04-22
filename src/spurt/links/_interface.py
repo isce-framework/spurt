47a3f8f26347725efebd145f716e9334d054fee3
piyushrpt <piyushrpt@users.noreply.github.com>
2024-04-21 17:19:06 -0700

    Adding a link interface

--- src/spurt/links/_interface.py
+++ src/spurt/links/_interface.py
@@ -40,55 +40,7 @@ class LinkModelInterface(Protocol):
     def ndim(self) -> int:
         """Return number of model parameters."""
 
-    def fwd_model(self, x: np.ndarray) -> np.ndarray:
-        """Return model evaluated at x."""
-
     def estimate_model(
-        self, wrapdata: np.ndarray, weights: np.ndarray | float | None = None
-    ) -> tuple[np.ndarray, float]:
-        """Return estimated model parameters and quality metric.
-
-        Parameters
-        ----------
-        wrapdata: np.ndarray
-            1D complex array or real valued array in radians.
-        weights: np.ndarray
-            1D array of same length as wrapdata with weights.
-            If scalar, all observations are equally weighted.
-
-        Returns
-        -------
-        params: np.ndarray
-              1D array of length ndim with the estimated model parameters.
-        quality: float
-              Temporal coherence or a similar quality metric indicating model
-              fit.
-        """
-
-    def estimate_model_many(
-        self,
-        wrapdata: np.ndarray,
-        weights: np.ndarray | float | None = None,
-        worker_count: int = 0,
-    ) -> tuple[np.ndarray, np.ndarray]:
-        """Estimate model for many inputs.
-
-        Parameters
-        ----------
-        wrapdata: np.ndarray
-            2D complex array or real valued array in radians.
-        weights: np.ndarray
-            1D / 2D array of same length as wrapdata with weights.
-            If 1D array, same weights are reused for all inputs.
-
-        Returns
-        -------
-        params: np.ndarray
-            2D array of shape (ninputs, ndim) with the estimated model parameters.
-        quality: np.ndarray
-            1D array of length ninputs representing temporal coherence or a similar
-            quality metric indicating model fit.
-        worker_count: int
-            Number of workers to use if inputs can be handled in parallel. The
-            implementation determines default in case a number <=0 is provided.
-        """
+        self, wrapdata: np.ndarray, weights: np.ndarray | None = None
+    ) -> tuple[float | np.ndarray, np.ndarray, float]:
+        """Return estimated model parameters, reconstructed model and quality metric."""
