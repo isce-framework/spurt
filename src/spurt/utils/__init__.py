90ae5448d5b8ab5645f8434e20a1cce9c9b24013
piyushrpt <piyushrpt@users.noreply.github.com>
2024-05-03 21:29:15 -0700

    Adding a logger

--- src/spurt/utils/__init__.py
+++ src/spurt/utils/__init__.py
@@ -6,7 +6,6 @@ from ._tiling import DensityTiler, RegularTiler, TilerInterface
 __all__ = [
     "get_cpu_count",
     "logger",
-    "merge",
     "TilerInterface",
     "RegularTiler",
     "DensityTiler",
